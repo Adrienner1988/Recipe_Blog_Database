3fe071b34620897f25a866bea137f9fcb7ddb11f
adrienner1988 <adriennerdaniels@gmail.com>
2024-07-14 13:07:36 -0400

    Fixed api route in urls.py, installed cors for django to get data to display, allowed frontend local server access."

--- urls.py
+++ urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import RecipeList, RecipeDetail
 
 urlpatterns = [
-    path('recipe/', RecipeList.as_view(), name='recipe-list'),
-    path('recipe/<int:pk>/', RecipeDetail.as_view(), name='recipe-detail'),
+    path('recipes/', RecipeList.as_view(), name='recipe-list'),
+    path('recipes/<id>/', RecipeDetail.as_view(), name='recipe-detail'),
 ]
