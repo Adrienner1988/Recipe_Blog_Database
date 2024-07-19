3fc2e4726127b188c41f19e58e8cdc178e672d3d
adrienner1988 <adriennerdaniels@gmail.com>
2024-07-19 03:25:37 -0400

    Removed submodules in recipe file

---
+++ recipe/views.py
@@ -0,0 +1,24 @@
+# Create your views here.
+from rest_framework import generics
+from .models import Recipe, Comment
+from .serializers import RecipeSerializer, CommentsSerializer
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+class RecipeList(generics.ListCreateAPIView):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+
+class RecipeDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+    lookup_field = 'pk' 
+
+class RecipeComments(generics.ListAPIView):
+    serializer_class = CommentsSerializer
+
+    def get_queryset(self):
+        recipe_id = self.kwargs['pk']
+        return Comment.objects.filter(recipe_id=recipe_id)
