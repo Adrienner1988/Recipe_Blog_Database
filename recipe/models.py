f149b58677f542b76414ed5df650e74682bec72d
adrienner1988 <adriennerdaniels@gmail.com>
2024-08-01 00:26:52 -0400

    Updated the modles.py Recipe table to add image field. Installed Pillow, a Python image library to help with the image processing.

--- recipe/models.py
+++ recipe/models.py
@@ -4,6 +4,7 @@ class Recipe(models.Model):
     title = models.CharField(max_length=100)
     ingredients = models.TextField()
     steps = models.TextField()
+    image = models.ImageField(upload_to='recipe/images/', null=True, blank=True)
 
     def __str__(self):
         return self.title
