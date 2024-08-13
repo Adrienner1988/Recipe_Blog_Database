4dca2a9ad1adbc8eb9c349c192ca2991508890d4
adrienner1988 <adriennerdaniels@gmail.com>
2024-08-13 19:20:36 -0400

    Added a Category class table.

--- recipe/models.py
+++ recipe/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='category/images/', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
 class Recipe(models.Model):
     title = models.CharField(max_length=100)
     ingredients = models.TextField()
     steps = models.TextField()
     image = models.ImageField(upload_to='recipe/images/', null=True, blank=True)
-    # category = models.CharField(max_length=100) 
+    category = models.ForeignKey(Category, related_name='recipes', on_delete=models.CASCADE) 
 
     def __str__(self):
         return self.title
@@ -17,3 +24,4 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
+
