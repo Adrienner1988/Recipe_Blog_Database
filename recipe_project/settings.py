70b5fc96672e02485e810cb91ee5eefdb80c163c
adrienner1988 <adriennerdaniels@gmail.com>
2024-09-19 13:56:51 -0400

    Setting up site to be hosted.

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -148,3 +149,4 @@ CORS_ALLOW_CREDENTIALS = True  # Allow cookies to be included
 CSRF_COOKIE_SAMESITE = 'None'
 CSRF_COOKIE_SECURE = True  # Ensure this is set if you're using HTTPS
 
+ALLOWED_HOSTS = ['recipe-rainbow.onrender.com']
